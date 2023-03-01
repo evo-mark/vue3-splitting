2957cece61b64e49c3bbf99bc2afb1bee8d13fd8
Craig Riley <c.riley@dsmdesign.co.uk>
2023-03-01 09:40:12 +0000

    Unref for computed config

--- demo/App.vue
+++ demo/App.vue
@@ -7,13 +7,16 @@
 import { useSplitting } from '../src';
 import { ref, computed } from 'vue';
 
+let wrapperClass = ref('font-bold');
+
 const sentence = ref('Hello World<br>This is a test<br>of the things to come');
 const config = computed(() => ({
 	lines: true,
 	words: false,
 	chars: true,
 	lineOffset: 10,
-	wrapperClass: 'font-bold'
+	wrapperClass: wrapperClass.value,
+	wrapperTag: 'span'
 }));
 
 setInterval(() => {
@@ -23,6 +26,7 @@ setInterval(() => {
 	} else {
 		sentence.value = defaultSentence;
 	}
+	wrapperClass.value = wrapperClass.value === 'font-bold' ? 'italic' : 'font-bold';
 	//config.value.lines = !config.value.lines;
 }, 5000);
 
@@ -32,5 +36,8 @@ const { Splitting: Header, counts } = useSplitting(sentence, config);
 <style>
 .font-bold {
 	font-weight: bold;
+}
+.italic {
+	font-style: italic;
 }
 </style>
